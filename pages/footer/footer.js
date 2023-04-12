585ba7a8bed39d9e6b49af1d0704721e0a9f12bb
aliDeveloperJPN-a12 <115926291+aliDeveloperJPN-a12@users.noreply.github.com>
2023-04-12 07:49:56 -0700

    fix bugs

---
+++ pages/footer/footer.js
@@ -0,0 +1,231 @@
+import Link from "next/link";
+export default function Footer() {
+  return (
+    <div className="w-full bg-white" dir="rtl">
+      {/* ---------------------------------------------------------------- */}
+
+      <div className="w-full xl:flex xl:justify-center xl:mt-[50px]">
+        {/* -------------------------------------------------------- */}
+
+        <div className="xl:w-[250px] h-[350px] p-3 fold:text-center">
+          <h5 className="xl:mb-6">برنامه های محبوب این ماه</h5>
+          <ul className="xl:text-[11px]">
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/34739/divar-android.html">
+                دانلود دیوار جدیدترین نسخه Divar 11.5.14 اندروید + نصب رایگان
+              </a>{" "}
+              <span></span>
+            </li>
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/117499/gbwhatsapp-android.html">
+                دانلود واتساپ جی بی GBWhatsApp 2022 جدید اندروید
+              </a>{" "}
+              <span></span>
+            </li>
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/112858/snapchat-android.html">
+                دانلود اسنپ چت جدید Snapchat 11.71.0.31 آپدیت اصلی اندروید
+              </a>{" "}
+              <span></span>
+            </li>
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/89808/shareit-android.html">
+                دانلود شریت SHAREit 6.8.38 جدید ۲۰۲۲ اندروید
+              </a>{" "}
+              <span></span>
+            </li>
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/149507/cafe-bazar-android.html">
+                دانلود بازار Bazaar 18.6.0 آپدیت جدید ۱۴۰۰ اندروید
+              </a>{" "}
+              <span></span>
+            </li>
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/169875/waze-gps-maps-traffic.html">
+                دانلود ویز اصلی Waze 4.81.0.4 برنامه مسیریاب اندروید
+              </a>{" "}
+              <span></span>
+            </li>
+          </ul>
+        </div>
+        {/* -------------------------------------------------------- */}
+
+        {/* -------------------------------------------------------- */}
+
+        <div className="xl:w-[250px] h-[350px] p-3 fold:text-center">
+          <h5 className="xl:mb-6">بازی های محبوب این ماه</h5>
+          <ul className="xl:text-[11px]">
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/34739/divar-android.html">
+                دانلود دیوار جدیدترین نسخه Divar 11.5.14 اندروید + نصب رایگان
+              </a>{" "}
+              <span></span>
+            </li>
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/117499/gbwhatsapp-android.html">
+                دانلود واتساپ جی بی GBWhatsApp 2022 جدید اندروید
+              </a>{" "}
+              <span></span>
+            </li>
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/112858/snapchat-android.html">
+                دانلود اسنپ چت جدید Snapchat 11.71.0.31 آپدیت اصلی اندروید
+              </a>{" "}
+              <span></span>
+            </li>
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/89808/shareit-android.html">
+                دانلود شریت SHAREit 6.8.38 جدید ۲۰۲۲ اندروید
+              </a>{" "}
+              <span></span>
+            </li>
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/149507/cafe-bazar-android.html">
+                دانلود بازار Bazaar 18.6.0 آپدیت جدید ۱۴۰۰ اندروید
+              </a>{" "}
+              <span></span>
+            </li>
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/169875/waze-gps-maps-traffic.html">
+                دانلود ویز اصلی Waze 4.81.0.4 برنامه مسیریاب اندروید
+              </a>{" "}
+              <span></span>
+            </li>
+          </ul>
+        </div>
+        {/* -------------------------------------------------------- */}
+        {/* -------------------------------------------------------- */}
+
+        <div className="xl:w-[250px] h-[350px] p-3 fold:text-center">
+          <h5 className="xl:mb-6">آخرین بازی ها و برنامه ها</h5>
+          <ul className="xl:text-[11px]">
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/34739/divar-android.html">
+                دانلود دیوار جدیدترین نسخه Divar 11.5.14 اندروید + نصب رایگان
+              </a>{" "}
+              <span></span>
+            </li>
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/117499/gbwhatsapp-android.html">
+                دانلود واتساپ جی بی GBWhatsApp 2022 جدید اندروید
+              </a>{" "}
+              <span></span>
+            </li>
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/112858/snapchat-android.html">
+                دانلود اسنپ چت جدید Snapchat 11.71.0.31 آپدیت اصلی اندروید
+              </a>{" "}
+              <span></span>
+            </li>
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/89808/shareit-android.html">
+                دانلود شریت SHAREit 6.8.38 جدید ۲۰۲۲ اندروید
+              </a>{" "}
+              <span></span>
+            </li>
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/149507/cafe-bazar-android.html">
+                دانلود بازار Bazaar 18.6.0 آپدیت جدید ۱۴۰۰ اندروید
+              </a>{" "}
+              <span></span>
+            </li>
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/169875/waze-gps-maps-traffic.html">
+                دانلود ویز اصلی Waze 4.81.0.4 برنامه مسیریاب اندروید
+              </a>{" "}
+              <span></span>
+            </li>
+          </ul>
+        </div>
+        {/* -------------------------------------------------------- */}
+        {/* -------------------------------------------------------- */}
+
+        <div className="xl:w-[250px] h-[350px] p-3 fold:text-center">
+          <h5 className="xl:mb-6">صفحات اصلی</h5>
+          <ul className="xl:text-[11px]">
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/34739/divar-android.html">
+                دانلود دیوار جدیدترین نسخه Divar 11.5.14 اندروید + نصب رایگان
+              </a>{" "}
+              <span></span>
+            </li>
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/117499/gbwhatsapp-android.html">
+                دانلود واتساپ جی بی GBWhatsApp 2022 جدید اندروید
+              </a>{" "}
+              <span></span>
+            </li>
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/112858/snapchat-android.html">
+                دانلود اسنپ چت جدید Snapchat 11.71.0.31 آپدیت اصلی اندروید
+              </a>{" "}
+              <span></span>
+            </li>
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/89808/shareit-android.html">
+                دانلود شریت SHAREit 6.8.38 جدید ۲۰۲۲ اندروید
+              </a>{" "}
+              <span></span>
+            </li>
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/149507/cafe-bazar-android.html">
+                دانلود بازار Bazaar 18.6.0 آپدیت جدید ۱۴۰۰ اندروید
+              </a>{" "}
+              <span></span>
+            </li>
+            <li className="xl:mb-4">
+              {" "}
+              <a href="https://androidkade.com/169875/waze-gps-maps-traffic.html">
+                دانلود ویز اصلی Waze 4.81.0.4 برنامه مسیریاب اندروید
+              </a>{" "}
+              <span></span>
+            </li>
+          </ul>
+        </div>
+        {/* -------------------------------------------------------- */}
+      </div>
+
+      {/* ---------------------------------------------------------------- */}
+      <div className="w-full p-4 bg-[#00304C]">
+        <Link href="#">
+          <span className="xl:mr-[40px]">
+            <img
+              src="https://androidkade.com/wp-content/uploads/2020/09/logo-1.png"
+              className="xl:w-[130px] xl:mt-[7px] xl:mr-[15px]"
+            />
+          </span>
+        </Link>
+        <p dir="rtl" className="xl:w-[1300px] xl:pt-[20px] text-white font-bold">
+          سایت اندرویدکده از سال 90 فعالیت خود را در زمینه دانلود بازی ها و
+          برنامه های اندرویدی آغاز کرد. همواره سعی ما بر آن بوده تا بتوانیم نیاز
+          مخاطبان هموطن را در استفاده از این سیستم عامل محبوب برطرف کنید. دانلود
+          رایگان برنامه های روز اندرویدی همچنین دانلود بازی های اچ دی در کمترین
+          زمان از خدمات سایت اندرویدکده بوده است
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Navbar from "./navbar/navbar";
+import Content from "./content/content";
+import Footer from "./footer/footer";
+function HomePage() {
+  return (
+    <div>
+      <Navbar/>
+      <Content/>
+      <Footer/>
+    </div>
+  );
+}
+export default HomePage;
